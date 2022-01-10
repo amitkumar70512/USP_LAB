f9ba0998ee6351f112b677ebc43fff5bf6fdc4bc
amit kumar <71318008+amitkumar70512@users.noreply.github.com>
2022-01-10 10:44:45 +0530

    Update script15.sh

--- usp_lab/Set3_program_13_to_16/program15/script15.sh
+++ usp_lab/Set3_program_13_to_16/program15/script15.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
 
-string="travel"
 
-
+echo "enter string"
+read str
+len=$(expr length $str)
 count=0
-
-a=`expr "$string" : '[^a]*a'`
- if [ $a -gt 0 ] ;then
-    count=$((count+1))
- 
-  fi
- 
-e=`expr "$string" : '[^e]*e'`
- if [ $e -gt 0 ] ;then
-    count=$((count+1))
-    
-  fi
-i=`expr "$string" : '[^i]*i'`
- if [ $i -gt 0 ] ;then
-    count=$((count+1))
-   
-  fi
-o=`expr "$string" : '[^o]*o'`
- if [ $o -gt 0 ] ;then
-    count=$((count+1))
-    
-  fi
-u=`expr "$string" : '[^u]*u'`
- if [ $u -gt 0 ] ;then
-    count=$((count+1))
-
-  fi
-
-echo "count of vowels in $string is $count"
+while [ $len -gt 0 ]
+do
+   ch=$(echo $str | cut -c $len)
+   case $ch in
+      [aeiouAEIOU] )
+         count=$(($count + 1))
+         echo $ch
+      ;;
+   esac
+   len=$(( $len - 1 ))
+done
+echo $count
